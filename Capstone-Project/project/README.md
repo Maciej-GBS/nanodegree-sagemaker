f13e3c2e7fd5254b3854290b66b8f9704affeaee
EC2 Default User <ec2-user@ip-172-16-147-247.ec2.internal>
2020-01-15 15:52:38 +0000

    Data loading update in Analysis notebook

--- Capstone-Project/project/README.md
+++ Capstone-Project/project/README.md
@@ -1,4 +1,5 @@
 # Project Details
+Here one can find all project notebooks and source code.
 
 ## indicators
 Module for applying various indicators on the dataset.
--- Capstone-Project/project/utils.py
+++ Capstone-Project/project/utils.py
@@ -5,3 +5,7 @@ Imports: pandas, numpy.
 """
 import pandas as pd
 import numpy as np
+
+def load_file_split(path):
+    """ Merges split .csv files into single DataFrame """
+    return pd.read_csv(path + 'columns-example.txt')
