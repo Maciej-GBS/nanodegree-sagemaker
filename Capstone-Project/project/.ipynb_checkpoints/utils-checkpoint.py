5e841be763265235a7b16b16f703a1a4ea50dfb8
EC2 Default User <ec2-user@ip-172-16-147-247.ec2.internal>
2020-01-18 15:04:18 +0000

    Data visualisation stage

--- Capstone-Project/project/.ipynb_checkpoints/utils-checkpoint.py
+++ Capstone-Project/project/.ipynb_checkpoints/utils-checkpoint.py
@@ -1,11 +1,15 @@
 """
 Utils containg space consuming, simple functions.
 
-Imports: pandas, numpy.
+Imports: os, pandas, numpy.
 """
 import pandas as pd
 import numpy as np
+import os
 
 def load_file_split(path):
     """ Merges split .csv files into single DataFrame """
-    return pd.read_csv(path + 'columns-example.txt')
+    names = pd.read_csv(path + 'columns-example.txt').columns
+    csv_files = [os.path.join(path, f) for f in os.listdir(path) if f[-4:] == '.csv']
+    parts = [pd.read_csv(csv, names=names, header=None) for csv in sorted(csv_files)]
+    return pd.concat(parts).reset_index(drop=True)
