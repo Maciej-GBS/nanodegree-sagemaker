644c3ac5456372851bfccb4c6bed7d8237c07221
EC2 Default User <ec2-user@ip-172-16-147-247.ec2.internal>
2020-01-26 16:01:26 +0000

    Finished convolution regressor architecture

--- Capstone-Project/project/model/model.py
+++ Capstone-Project/project/model/model.py
@@ -3,44 +3,55 @@ import torch
 import torch.nn
 
 class LSTMRegressor(torch.nn.Module):
-    def __init__(self, input_size, input_channels, c_filters, c_kernel_size, lstm_size, lstm_layers):
+    def __init__(self, input_size, input_channels, c_filters, c_kernel_size, lstm_layers, lstm_hidden, output_size):
         super().__init__()
-        
         # Each channel is a different column of our data
-        # So input of 10 bars history has a shape of (N,C,L) = (len,batch,features) = (N,n_columns,10)
+        # So input of 11 bars history has a shape of (N,C,L) = (len,batch,features) = (N,n_columns,11)
         c_out_channels = input_channels * c_filters
         self.conv = torch.nn.Conv1d(in_channels=input_channels, out_channels=c_out_channels, kernel_size=c_kernel_size)
-        c_out_size = c_out_channels * (input_size - c_kernel_size + 1)
-        self.lstm = torch.nn.LSTM(input_size=c_out_size, hidden_size=lstm_size, num_layers=lstm_layers, dropout=0.2)
-        
-        # Apply a dense linear transformation with additional last Close value input
-        self.dense = torch.nn.Linear(in_features=lstm_size+1, out_features=1)
+        c_out = input_size - c_kernel_size + 1
+        self.lstm = torch.nn.LSTM(input_size=c_out, hidden_size=lstm_hidden, num_layers=lstm_layers, dropout=0.2)
+        lstm_flat_out = lstm_hidden * c_out_channels
+        self.dense = torch.nn.Linear(in_features=lstm_flat_out, out_features=output_size)
     
     def forward(self, x):
-        x = x.transpose(-2, -1)
         print("===={0}=====\n{1}\n==========".format("x",x.shape))
-        conv_out = self.conv(x)
+        conv_out = self.conv(x.transpose(-2,-1))
         print("===={0}=====\n{1}\n==========".format("conv_out",conv_out.shape))
-        lstm_out, _ = self.lstm(conv_out.flatten(start_dim=1))
-        
+        lstm_out, _ = self.lstm(conv_out)
+        lstm_out = lstm_out.flatten(start_dim=1)
         print("===={0}=====\n{1}\n==========".format("lstm_out",lstm_out.shape))
-        # Append Close price
-        dense_in = torch.cat((lstm_out, x[3][-1]), dim=0)
-        print("===={0}=====\n{1}\n==========".format("x3",x[3][-1]))
+        #dense_in = torch.cat((lstm_out.transpose(-2,-1), x[:,-1,3].reshape(lstm_out.shape[0], lstm_out.shape[2], 1)), dim=2).transpose(-2,-1)
+        dense_in = lstm_out
         print("===={0}=====\n{1}\n==========".format("dense_in",dense_in.shape))
-        dense_out = self.dense(dense_in)
+        dense_out = self.dense(dense_in).squeeze()
         return dense_out
     
 class LSTMBatchRegressor(torch.nn.Module):
-    def __init__(self, num_features, hidden_dim):
+    # TODO: This is a copy of CONV Regressor !!!
+    def __init__(self, input_size, input_channels, c_filters, c_kernel_size, lstm_layers, lstm_hidden, output_size):
         super().__init__()
-        # TODO: Create torch nn layers
-        self.norm = torch.nn.BatchNorm1d(num_features)
-        self.lstm = torch.nn.LSTM(num_features, hidden_dim)
+        # Each channel is a different column of our data
+        # So input of 11 bars history has a shape of (N,C,L) = (len,batch,features) = (N,n_columns,11)
+        c_out_channels = input_channels * c_filters
+        self.conv = torch.nn.Conv1d(in_channels=input_channels, out_channels=c_out_channels, kernel_size=c_kernel_size)
+        c_out = input_size - c_kernel_size + 1
+        self.lstm = torch.nn.LSTM(input_size=c_out, hidden_size=lstm_hidden, num_layers=lstm_layers, dropout=0.2)
+        lstm_flat_out = lstm_hidden * c_out_channels
+        self.dense = torch.nn.Linear(in_features=lstm_flat_out, out_features=output_size)
     
     def forward(self, x):
-        # TODO: create a correct forward
-        pass
+        print("===={0}=====\n{1}\n==========".format("x",x.shape))
+        conv_out = self.conv(x.transpose(-2,-1))
+        print("===={0}=====\n{1}\n==========".format("conv_out",conv_out.shape))
+        lstm_out, _ = self.lstm(conv_out)
+        lstm_out = lstm_out.flatten(start_dim=1)
+        print("===={0}=====\n{1}\n==========".format("lstm_out",lstm_out.shape))
+        #dense_in = torch.cat((lstm_out.transpose(-2,-1), x[:,-1,3].reshape(lstm_out.shape[0], lstm_out.shape[2], 1)), dim=2).transpose(-2,-1)
+        dense_in = lstm_out
+        print("===={0}=====\n{1}\n==========".format("dense_in",dense_in.shape))
+        dense_out = self.dense(dense_in).squeeze()
+        return dense_out
 
 def model_fn(model_dir):
     """Load the PyTorch model from the `model_dir` directory."""
