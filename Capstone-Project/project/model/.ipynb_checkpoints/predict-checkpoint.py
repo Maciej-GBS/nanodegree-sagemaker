a87a7e189552afca89bfd85e878715d37649a220
EC2 Default User <ec2-user@ip-172-16-136-12.ec2.internal>
2020-01-31 20:03:08 +0000

    Test prediction serving

--- Capstone-Project/project/model/.ipynb_checkpoints/predict-checkpoint.py
+++ Capstone-Project/project/model/.ipynb_checkpoints/predict-checkpoint.py
@@ -23,31 +23,38 @@ def convert(data, meta, ema=0.03, sma=70, rsi=14):
 
 def input_fn(serialized_input_data, content_type):
     """ Input should be text symbol to predict on (e.g. EURUSD) """
-    if content_type != 'application/json':
+    if content_type != 'text/plain':
         raise Exception('Requested unsupported ContentType in content_type: ' + content_type)
     print("> Received symbol information")
     
-    sym_info = json.loads(serialized_input_data)
+    symbol = serialized_input_data.decode('utf-8')
     interval = '1d'
     period = '6mo'
-    sym = yf.Ticker(sym_info['symbol'])
+    sym = yf.Ticker(symbol)
     data = sym.history(period=period, interval=interval)
     return convert(data, model_info['input_channels'])
 
 def output_fn(prediction_output, accept):
     print("> Responding with forecast")
-    return str(prediction_output)
+    d = {
+        'open':predcition_output[0,:,0].tolist(),
+        'high':predcition_output[0,:,1].tolist(),
+        'low':predcition_output[0,:,2].tolist(),
+        'close':predcition_output[0,:,3].tolist(),
+        'pred':predcition_output[1].tolist(),
+        }
+    return json.dumps(d)
 
 def predict_fn(input_data, model):
     print('> Inferring forecast for provided symbol')
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
-    # TODO: make this work
-    data = input_data.to(device)
+    # Expected size is (1, input_size, input_channels)
+    data = input_data[-model_info['input_size']:].unsqueeze(dim=0).to(device)
 
     # Model into evaluation mode
     model.eval()
     with torch.no_grad():
         result = model(data)
-        result = np.array(result)
-    return result
+        result = np.array(denormalize(result, data[:,:,3]))
+    return np.array([data, result])
